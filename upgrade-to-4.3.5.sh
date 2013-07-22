f4046487772b63c9acfef7531871845f0ab29123
Frank J. Gómez <fgomez@confluencecorp.com>
2013-07-22 15:54:48 -0400

    Update message templates post-upgrade.

--- upgrade-to-4.3.5.sh
+++ upgrade-to-4.3.5.sh
@@ -20,3 +20,6 @@ tar -C "${WEBROOT}"/sites/all/modules/ -xzf "${ABS_CALLPATH}/${MY_TARGZ}"
 
 echo -e "\E[31mUpgrading the DB from the command line does not appear to work. Point your browser to /?q=civicrm/upgrade&reset=1.\033[0m"
 read -sn 1 -p "Once you're done, press any key to continue..."; echo
+
+echo "Updating message templates..."
+mysql chorusad_civic3x < "${ABS_CALLPATH}/update-message-templates.sql"
